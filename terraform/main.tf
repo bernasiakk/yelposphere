b2881cd119176a93f1ba685b532fa0d2a8c80925
bernasiakk <bernasiakk@gmail.com>
2024-05-24 12:53:09 +0000

    updated comments in "main.tf" file

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ terraform {
 provider "google" {
   project = var.project_id
   region  = var.region
-  // credentials = file(var.credentials)  # Use this if you do not want to set env-var GOOGLE_APPLICATION_CREDENTIALS
+  // credentials = file(var.credentials)  # Use this if you do not want to set env-var GOOGLE_APPLICATION_CREDENTIALS. I am using the env-var, so it's ok...
 }
 
 module "apis" {
